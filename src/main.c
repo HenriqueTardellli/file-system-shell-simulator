1a091e142656e478524032b84794ad6981a9c9fd
Henrique <henrique2004pt@hotmail.com>
2026-02-01 19:14:33 +0000

    feat: Coded main to test preciously created functions and data structures

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
+#include <stdio.h> 
 #include "fs_logic.h"
 
-int main(){
+Folder* root;		//Pointer to the root of the system
+Folder* cur;		//Pointer to the current directory the user is on
 
-	ola_mundo_sistema_de_arquivos();
+int main(){
+	printf("----- Initializing file system -----");
+
+	root = create_system();
+	cur = root;			//Start off by poiting to the root
+	
+	if (root != NULL){
+		printf("System was built successfully");
+		printf("Current directory: %s\n", cur->name);
+	}
+
+	// TODO Add loops for the menu
+	
+	destroy_system(root);
+
 	return 0;
 }
