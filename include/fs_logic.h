8207e3771b89472888eb35ea6f902476e3b62ae5
Henrique <henrique2004pt@hotmail.com>
2026-02-01 17:52:44 +0000

    feat: Development of the header file for the main data structures

--- include/fs_logic.h
+++ include/fs_logic.h
@@ -1,6 +1,38 @@
 #ifndef FS_LOGIC_H
 #define FS_LOGIC_H
 
-void ola_mundo_sistema_de_arquivos();
+/*Constants to limit the size of the data*/
+#define MAX_NAME 50
+#define MAX_CONTENT 200
+
+
+/*Structure for the text files, which are the tree leaves*/
+typedef struct File{
+	char name[MAX_NAME];
+	char content[MAX_CONTENT];
+	struct File* next;
+}File;
+
+/*Structur for folders, nodes of the tree*/
+
+typedef struct Folder {
+	char name[MAX_NAME];
+	
+	//Vertical hierarchical navigation 
+	struct Folder* parent;
+	struct Folder* F_child;		/*The first folder, or more precisely, the first element of the linked list*/
+
+	//Horizontal navigation
+	struct Folder* next;		/*Points to the nest element in the horizontal linked list*/
+
+	//Text files
+	struct File* files;		/*Points to the linked list of text files in the current folder*/
+}Folder;
+
+/*Main functions prototypes*/
+
+Folder* create_system();
+
+void destroy_system(Folder* folder);
 
 #endif
