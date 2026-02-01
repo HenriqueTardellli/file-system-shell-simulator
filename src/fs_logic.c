df182b2806b275c58f61fd717efc7d4e898ab240
Henrique <henrique2004pt@hotmail.com>
2026-02-01 18:52:42 +0000

    feat: Folder add/remove functions logic implementation

--- src/fs_logic.c
+++ src/fs_logic.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
 #include "fs_logic.h"
 
-void ola_mundo_sistema_de_arquivos(){
 
-	printf("Olá !\n");
+static Folder* create_folder(char* name, Folder* parent){
+	Folder* new_folder = (Folder*)mallloc(sizeof(Folder));
 
+	if (new_folder == NULL){
+		printf("Error: Failed to allocate memory.\n");
+		exit(1);
+	}
+
+	//Initialize data
+	strncpy(new_folder->name, name, MAX_NAME);	//Copy the data from the name argument into the memory addres
+							//of the name atribute
+	new_folder->parent = parent;
+	
+	//Initialize pointers as NULL, to avoid pointing to "garbage" data
+	new_folder->F_child = NULL;
+	new_folder->next = NULL;
+	new_folder->files = NULL;
+
+	return new_folder;
+}
+
+Folder* create_system(){
+	return create_folder("root", NULL);		//We give as argument for the parent Null since it's the root of the system
+}
+
+void destroy_system(*Folder folder){
+	//TODO: Implementar função de limpeza
+	if (folder != NULL){
+		printf("DEBUG: A destruir sistema a partir de %s... (Lógica pendente)\n", folder->nome");
+		free(folder);
+	}
 }
